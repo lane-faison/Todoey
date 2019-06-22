3ed50c327c4f2fe5b5304d58d1aec8a1577cb776
Lane Faison <lfaison@tangogroup.com>
2019-06-22 10:17:22 -0600

    Encodes and Decodes

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -10,17 +10,14 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = [String]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
+    var itemArray = [Item]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
-        
+        loadItems()
     }
     
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
@@ -30,12 +27,14 @@ class ToDoListViewController: UITableViewController {
         let alertController = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let addAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            guard let newItem = textField.text else { return }
+            guard let title = textField.text else { return }
+            
+            let newItem = Item()
+            newItem.title = title
+            
             self.itemArray.append(newItem)
             
-            self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
-            
-            self.tableView.reloadData()
+            self.saveItems()
         }
         
         alertController.addTextField { (alertTextField) in
@@ -47,6 +46,29 @@ class ToDoListViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    private func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array: \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    private func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array: \(error)")
+            }
+        }
+    }
 }
 
 // MARK: - TableView Datasource Methods
@@ -57,16 +79,19 @@ extension ToDoListViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
---
+++ Todoey/Data Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lane Faison on 6/21/19.
+//  Copyright © 2019 Lane Faison. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
